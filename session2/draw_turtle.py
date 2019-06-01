0af5f06307d2b9196285d5da79c0f2cb30bb404b
Đinh Huy Minh <dhuyminh@outlook.com.vn>
2019-06-01 22:44:28 +0700

    session 2

---
+++ session2/draw_turtle.py
@@ -0,0 +1,22 @@
+from turtle import *
+shape("turtle")
+speed(-1)
+for i in range (18):
+ forward(100)
+ left(90)
+ forward(100)
+ left(90)
+ forward(100)
+ left(90)
+ forward(100)
+
+ left (30)
+mainloop()
+
+# from turtle import *
+# shape("turtle")
+# speed (-1)
+# for i in range (20, 200, 5):
+#     forward(100 +i)
+#     left(90)
+# mainloop()
---
+++ session2/variable.py
@@ -0,0 +1,19 @@
+# # print("learning variable")
+# name = "Huy Minh"
+# name = "asdads"
+# print("name: ",name)
+# print("type name: ", type(name))
+
+#  age = 16
+#  print("age: ",age)
+#  print("type age: ", type(age))
+# dob = 2003
+# year = 2019
+# age = 0
+# age = year - dob
+# print(age)
+
+year_born= input("enter your year of birth: ")
+year_current= input("enter current year: ")
+age = int(year_current) - int(year_born)
+print("age: ", age)
